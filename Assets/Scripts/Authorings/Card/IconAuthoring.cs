6e29b9ec565d9cc7acb1735ecf70dff6eef35031
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-05-21 12:06:43 +0200

    ✨ Add card icon script

---
+++ Assets/Scripts/Authorings/Card/IconAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using UnityEngine.Serialization;
+
+public class IconAuthoring : MonoBehaviour
+{
+    [SerializeField] private Sprite _sprite;
+    
+    class Baker : Baker<IconAuthoring>
+    {
+        public override void Bake(IconAuthoring authoring)
+        {
+            var icon = new Icon
+            {
+                Sprite = authoring._sprite
+            };
+            AddComponentObject(GetEntity(authoring, TransformUsageFlags.None), icon);
+        }
+    }
+}
