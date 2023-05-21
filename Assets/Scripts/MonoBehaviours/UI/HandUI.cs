80af89339bbbd7016ab9e7950907c0534139f50a
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-05-21 22:48:34 +0200

    💄 Display in the card's UI how many uses are left

--- Assets/Scripts/MonoBehaviours/UI/HandUI.cs
+++ Assets/Scripts/MonoBehaviours/UI/HandUI.cs
@@ -1,11 +1,20 @@
-﻿using Unity.Entities;
+﻿using System;
+using TMPro;
+using Unity.Entities;
 using UnityEngine;
 using UnityEngine.UI;
 using Deck = Unity.Entities.DynamicBuffer<CardInDeck>;
 
 public class HandUI : MonoBehaviour
 {
-    [SerializeField] private Image[] _cardsIcons = new Image[2];
+    [SerializeField] private Card[] _cards = new Card[2];
+
+    [Serializable]
+    private struct Card
+    {
+        public Image Icon;
+        public TextMeshProUGUI LeftUsesText;
+    }
 
     private void Update()
     {
@@ -15,10 +24,13 @@ public class HandUI : MonoBehaviour
         if(handQuery.TryGetSingleton<Hand>(out var hand) && handQuery.TryGetSingletonBuffer<CardInDeck>(out var deck))
         {
             var cardsInHand = hand.GetCardsInHand(deck);
-            for (var i = 0; i < _cardsIcons.Length && i < cardsInHand.Length; i++)
+            for (var i = 0; i < _cards.Length && i < cardsInHand.Length; i++)
             {
-                if(_cardsIcons[i] != null)
-                    _cardsIcons[i].sprite = entityManager.GetComponentData<Icon>(cardsInHand[i].CardPrefab).Sprite;
+                if(_cards[i].Icon != null)
+                    _cards[i].Icon.sprite = entityManager.GetComponentData<Icon>(cardsInHand[i].CardPrefab).Sprite;
+
+                if (_cards[i].LeftUsesText != null && cardsInHand[i].LeftUseCount != CardInDeck.INVALID_LEFT_USE_COUNT)
+                    _cards[i].LeftUsesText.text = "x" + cardsInHand[i].LeftUseCount;
             }
         }
     }
