6e40163df89fbf0e6f955f43c6c4f1581ff75466
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-05-26 22:23:51 +0200

    🐛 Improve the connection of an equipment object created with a card

--- Assets/Scripts/Systems/Equipment/ConnectJointToCardUserSystem.cs
+++ Assets/Scripts/Systems/Equipment/ConnectJointToCardUserSystem.cs
@@ -1,15 +1,18 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 
+[UpdateAfter(typeof(CardInHandUserSystem))]
+[UpdateInGroup(typeof(UseCardInHandSystemGroup))]
 [BurstCompile]
 public partial struct ConnectJointToCardUserSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (children, cardUsedBy, entity) in SystemAPI.Query<DynamicBuffer<Child>, CardUsedBy>().WithAll<ConnectJointToCardUser>().WithEntityAccess())
+        foreach (var (children, cardUsedBy, transform, entity) in SystemAPI.Query<DynamicBuffer<LinkedEntityGroup>, CardUsedBy, RefRW<LocalTransform>>().WithAll<ConnectJointToCardUser>().WithEntityAccess())
         {
             foreach (var child in children)
             {
@@ -18,6 +21,9 @@ public partial struct ConnectJointToCardUserSystem : ISystem
                     var physicsConstrainedBodyPair = SystemAPI.GetComponentRW<PhysicsConstrainedBodyPair>(child.Value, false);
                     var bodyPartsReferenceToConnect = SystemAPI.GetComponent<BodyPartsReference>(cardUsedBy.UsedBy);
                     physicsConstrainedBodyPair.ValueRW = new PhysicsConstrainedBodyPair(physicsConstrainedBodyPair.ValueRO.EntityA, bodyPartsReferenceToConnect.RightLowerArm, false);
+                    var joint = SystemAPI.GetComponent<PhysicsJoint>(child.Value);
+                    var usedByTransform = SystemAPI.GetComponent<LocalToWorld>(bodyPartsReferenceToConnect.RightLowerArm);
+                    transform.ValueRW.Position = math.mul(joint.BodyBFromJoint.AsRigidTransform().rot, joint.BodyBFromJoint.Position) + usedByTransform.Position;
                     SystemAPI.SetComponentEnabled<ConnectJointToCardUser>(entity, false);
                     break;
                 }
